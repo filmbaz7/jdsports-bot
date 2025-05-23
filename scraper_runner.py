46ea890e3b7abe4e209e7615049be819f7946170
filmbaz7 <farzadomidvar76@gmail.com>
2025-05-23 15:29:25 -0700

    Update scraper_runner.py

--- scraper_runner.py
+++ scraper_runner.py
@@ -1,23 +1,8 @@
-import json
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from jdsports_spider import JDSportsSpider
+from jdsports.spiders.jdsports_spider import JDSportsSpider
 
 def run_spider():
-    items = []
-
-    class CustomSpider(JDSportsSpider):
-        def parse(self, response):
-            for item in super().parse(response):
-                if item["discount"] >= 30:
-                    items.append(item)
-
     process = CrawlerProcess(get_project_settings())
-    process.crawl(CustomSpider)
+    process.crawl(JDSportsSpider)
     process.start()
-    return items
-
-if __name__ == "__main__":
-    data = run_spider()
-    with open("output.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
